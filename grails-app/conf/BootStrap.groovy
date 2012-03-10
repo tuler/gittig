569f71ea1c1ce18c1669416dbccd2a603e75dcff
Danilo Tuler <tuler@pobox.com>
2012-03-10 13:26:30 -0300

    creating admin user in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
+import git.mirror.*
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+		if (Role.count() == 0) {
+			log.info "Creating standard roles"
+			['ROLE_ADMIN', 'ROLE_USER'].each { role -> 
+				new Role(authority: role).save(failOnError: true)
+			}
+		}
+		
+		if (User.count() == 0) {
+			log.info "Creating 'admin' user"
+			def user = new User(username: 'admin', userRealName: 'admin', password: 'admin', enabled: true, email: 'admin@servername').save(failOnError: true)
+			UserRole.create(user, Role.findByAuthority('ROLE_ADMIN'), true)
+		}		
+		
+	}
+	def destroy = {
+	}
 }
