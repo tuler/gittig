7cefd738b623f42d41efd4d14b0cfcb5deb95c00
Danilo Tuler <tuler@pobox.com>
2012-05-04 08:34:14 -0300

    reloading the page if there is a new job

--- web-app/js/queue.js
+++ web-app/js/queue.js
@@ -3,16 +3,29 @@
 	var updateStatus = function() {
 		$.getJSON('status', function(data) {
 			_.each(data.jobs, function(job) {
+				// find the job row
 				var tr = $('#' + job.id);
+				if (tr.length == 0) {
+					// job not found, reload the page
+					location.reload();
+				}
+				
+				// get the status column
 				var td = $('td.status', tr);
+				
+				// hide the div of all the different status
 				$('div.status', td).hide();
+				
+				// show the correct div
 				$('div.' + job.status, td).show();
 				
 				if (job.status == 'RUNNING') {
+					// job running, update the progress bar
 					$('.title', td).html(job.title);
-					job.progress = 0.5;
 					$('.bar', td).css('width', (job.progress * 100) + '%');
+					
 				} else if (job.status == 'ERROR') {
+					// job error, update the error popover
 					$('.ERROR span', td).data('content', job.error).popover({
 						placement: 'bottom'
 					});
